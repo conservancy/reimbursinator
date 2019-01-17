91c0b8eb5e899f2d8987f2f50ac57f3e32eafd47
Preston Doman <pdoman@pdx.edu>
2019-01-17 13:12:17 -0800

    Add logout script to dashboard.html and fix formatting

---
+++ front/static/logout.js
@@ -0,0 +1,33 @@
+function postToLogoutEndpoint(event) {
+    event.preventDefault();
+
+    const token = localStorage.getItem("token");
+    const url = "https://reqres.in/api/logout" // mock api service
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Authorization", `Token  ${token}`);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                console.log("LOGOUT SUCCESS!");
+                console.log(`Server response:\n${this.response}`);
+                localStorage.removeItem("token");
+                window.location.replace("index.html");
+            } else {
+                console.log("LOGOUT FAILURE!");
+                console.log(`Server status: ${this.status}`);
+                console.log(`Server response:\n${this.response}`);
+            }
+        }
+    };
+
+    xhr.onerror = function() {
+        alert("Error connecting to authentication server!");
+    };
+
+    xhr.send();
+}
+
+const logoutLink = document.querySelector("#logoutLink");
+logoutLink.addEventListener("click", postToLogoutEndpoint);
