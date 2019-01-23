63f3465ca4c4cca1bf0ff5ece0a3293004fbc0de
Daniel Dupriest <kououken@gmail.com>
2019-01-22 17:03:07 -0800

    Created initial user and some sample report objects. Initialized superuser to admin/admin

--- back/backend/admin.py
+++ back/backend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Report)
+admin.site.register(Section)
+admin.site.register(Field)
+admin.site.register(DataBool)
+admin.site.register(DataDecimal)
+admin.site.register(DataDate)
+admin.site.register(DataFile)
+admin.site.register(DataString)
+admin.site.register(DataInteger)
--- back/users/admin.py
+++ back/users/admin.py
@@ -8,5 +8,6 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name']
 
 admin.site.register(CustomUser, CustomUserAdmin)
