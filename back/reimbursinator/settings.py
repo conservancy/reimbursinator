bd1bf440596c4035fda1bcf3b01d1860b6d71132
Daniel Dupriest <kououken@gmail.com>
2019-01-22 16:38:09 -0800

    Created CustomUser and tweaked backend models.

--- back/reimbursinator/settings.py
+++ back/reimbursinator/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -82,6 +83,9 @@ DATABASES = {
     }
 }
 
+# Authentication
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
